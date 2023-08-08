3dfd54f8e14c304f6727b3a6a47c37c46a6e90ec
Harsh Agrawal <122919453+Harsh9174@users.noreply.github.com>
2023-08-09 00:02:30 +0530

    Update Hr_name.py

--- coading files/Hr_name.py
+++ coading files/Hr_name.py
@@ -1,33 +1,35 @@
 import pandas as pd
-import numpy as np
 import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 
-
+# Load data from a CSV file
 df = pd.read_csv("A:/Masai Projects/Job Analytics project/Job_Data1.csv")
+
+# Drop rows with missing values and save the cleaned data to a new CSV file
 df.dropna(inplace=True)
-df.to_csv("A:/Masai Projects/Job Analytics project/final_data.csv",index=False)
+df.to_csv("A:/Masai Projects/Job Analytics project/final_data.csv", index=False)
 
-
+# Extract the 'Link' column values from the DataFrame
 Link_L = []
 for i in df['Link']:
     Link_L.append(i)
+
+# Set up Selenium WebDriver using Chrome
 service = Service('C:/Users/Mi/Downloads/chromedriver_win32/chromedriver.exe')
 driver = webdriver.Chrome(executable_path=r'C:/Users/Mi/Downloads/chromedriver_win32/chromedriver.exe')
 
 HR_name = []
 
-# link = 'https://www.instahyre.com/job-245061-data-scientist-engineering-at-google-2-bangalore-hyderabad/'
+# Iterate through the list of links
 try:
     for link in Link_L:
         driver.get(link)
+        
+        # Find the HR name element on the page and append to HR_name list
         try:
-            hr_name = driver.find_element(By.CLASS_NAME,"rec-name").text
+            hr_name = driver.find_element(By.CLASS_NAME, "rec-name").text
             HR_name.append(hr_name)
         except:
             HR_name.append("NA")
@@ -35,7 +37,9 @@ try:
 except:
     print("NA")
 
-df = pd.DataFrame({"HR_Name":HR_name})
+# Create a new DataFrame with HR names
+df_hr = pd.DataFrame({"HR_Name": HR_name})
 
-df.to_csv("A:/Masai Projects/Job Analytics project/hr_exp.csv",index = False)
-print(df)
+# Save the new DataFrame to a CSV file
+df_hr.to_csv("A:/Masai Projects/Job Analytics project/hr_exp.csv", index=False)
+print(df_hr)
